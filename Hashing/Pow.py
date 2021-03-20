f8fbdcc9148b07d771199f2ddefa0fe26d063c71
benj224 <52797746+benj224@users.noreply.github.com>
2021-03-20 16:22:39 +0000

    Create Pow.py

---
+++ Hashing/Pow.py
@@ -0,0 +1,7 @@
+##work out what mixture of algorithms to use
+
+
+##basic idea#
+##input a number
+##go through multiple hashing algorithms to get the end number
+##if that number has a specific quality e.g. starts with lots of zeros, then it is a good block
