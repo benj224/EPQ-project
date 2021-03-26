5fff914a04d6a5c376a24baba7e9b7dfb9255106
15bmason <80707961+15bmason@users.noreply.github.com>
2021-03-26 13:47:35 +0000

    oof

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello")
+import random
+import hashlib
+import string
+
+x = "".join(random.choice(string.ascii_lowercase) for i in range(10))
+x = hashlib.sha256(x.encode()).hexdigest()
+difficulty = "000"
+while x[:3] != difficulty:
+    x = "".join(random.choice(string.ascii_lowercase) for i in range(10))
+    print("Unhashed: ", x)
+    x = hashlib.sha256(x.encode()).hexdigest()
+    print("Hashed: ", x)
